ae6ea8202cbf1ca4603bce23cd8ab2bcc82d3f47
JohanDVP <johanvperez123@gmail.com>
2026-03-01 19:30:42 -0500

    excepciones importantes

---
+++ src/app/exceptions.py
@@ -0,0 +1,16 @@
+class ErrorInesperado(Exception): #Excepcion base
+    pass
+
+class UsuarioNoEncontradoError(Exception): #Si no existe el user
+    pass
+
+class RoomNoEncontradaError(Exception): #Si no se encuentra el id de la habitacion
+    pass
+
+class RoomNoDisponibleError(Exception): # Si la habitacionque intenta tomar ya esta ocupada
+    pass
+
+class ReservaNoEncontradaError(Exception): #Si no existe una reserva con ese id
+    pass
+
+
